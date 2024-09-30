a5d6223a8777a894139df7ec4646b8974a4cd685
Roman <roman.sceaux@etu.univ-lehavre.fr>
2024-09-30 19:48:02 +0200

    ajout difference prix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     Promise.all([
-        fetch('data/items.json').then(response => response.json()),
-        fetch('data/itemTypes.json').then(response => response.json())
+        fetch('./data/items.json').then(response => response.json()),
+        fetch('./data/itemTypes.json').then(response => response.json())
     ])
     .then(([itemsData, itemTypesData]) => {
         const tableBody = document.querySelector('table tbody');
@@ -9,33 +9,36 @@ document.addEventListener("DOMContentLoaded", function () {
             const item = itemsData[key];
             const priceKeys = Object.keys(item.price);
             const lastPriceKey = priceKeys[priceKeys.length - 1];
-            const lastPrice = item.price[lastPriceKey - 1].value;
-            const lastPriceDate = item.price[lastPriceKey - 1].date;
+            const lastPrice = item.price[lastPriceKey].value;
+            const lastPriceDate = item.price[lastPriceKey].date;
 
             const dateDiffInDays = (dateString) => {
                 const date = new Date(dateString);
                 const now = new Date();
                 const diffTime = Math.abs(now - date);
-                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-                return `${lastPrice} (${diffDays} jours)`;
+                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))-1;
+                let sRet = `${lastPrice.toLocaleString('fr-FR')} (${diffDays} jour`;
+                return diffDays > 1 ? sRet + "s)" : sRet + ")";
             };
             const lastPriceDateDiff = dateDiffInDays(lastPriceDate);
+            console.log(lastPrice)
 
             moyenne_prix = 0;
             priceKeys.forEach(element => {
-                moyenne_prix += parseInt(parseInt(item.price[element].value.replace(/\s/g, '')));
+                moyenne_prix += parseInt(parseInt(item.price[element].value));
             });
             moyenne_prix = parseInt(moyenne_prix / priceKeys.length);
 
-            const className = moyenne_prix > 0 ? "augmentationPrix" : "diminutionPrix";
+            const className = moyenne_prix > 0 ? "diminutionPrix" : "augmentationPrix";
             const row = document.createElement('tr');
             row.innerHTML = `
                 <td><img src="https://vertylo.github.io/wakassets/itemTypes/${itemTypesData[item.type]}.png" alt="${item.type}" /></td>
                 <td>${key}</td>
                 <td>${lastPriceDateDiff}</td>
-                <td>${moyenne_prix}</td>
-                <td class="${className}">${item.difference}</td>
+                <td>${moyenne_prix.toLocaleString('fr-FR')}</td>
+                <td class="${className}">${(moyenne_prix-parseInt(lastPrice)).toLocaleString('fr-FR')}</td>
             `;
+            console.log(parseInt(lastPrice) + " -- " + moyenne_prix)
             tableBody.appendChild(row);
         });
     })
